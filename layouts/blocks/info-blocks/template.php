44f31c94bf301220ab1cab940dbcbbfe6154f69d
Tsereteli.Ph <filipptsereteli@yandex.ru>
2023-10-28 19:56:53 +0300

    add and done .info-blocks

---
+++ layouts/blocks/info-blocks/template.php
@@ -0,0 +1,39 @@
+<section class="info-blocks">
+	<div class="container info-blocks__container">
+		<?php
+			get_template_part('/layouts/partials/title', null, array(
+				'class' => 'info-blocks__title',
+				'title' => get_sub_field('title')
+			));
+
+			$infoBlocks = get_sub_field( 'info_blocks' );
+			if ( $infoBlocks ) :
+		?>
+			<ul class="reset-list info-blocks__list">
+				<?php foreach ( $infoBlocks as $item ) : ?>
+					<li class="info-blocks__item">
+						<?php if ( $item['title'] ) : ?>
+							<h2 class="title info-blocks__item-title"><?php echo $item['title']; ?></h2>
+						<?php endif; ?>
+
+						<div class="info-blocks__item-separator"></div>
+
+						<?php if ( $item['text'] ) : ?>
+							<div class="info-blocks__item-text"><?php echo $item['text']; ?></div>
+						<?php endif; ?>
+
+						<?php
+							$link = $item['link'];
+							if ( $link ) :
+						?>
+							<a href="<?php echo $link['url']; ?>" class="btn info-blocks__item-link" target="<?php echo $link['target']; ?>">
+								<?php echo ( $link['title'] ) ? $link['title'] : 'Read more'; ?>
+								<svg width="17" height="17"><use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/images/sprite.svg#icon-arrow"></use></svg>
+							</a>
+						<?php endif; ?>
+					</li>
+				<?php endforeach; ?>
+			</ul>
+		<?php endif; ?>
+	</div>
+</section>
